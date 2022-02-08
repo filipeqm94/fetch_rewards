714ce7441bc934a4b91dad73ea70f2332a489472
filipeqm94 <queiroz.filipe94@gmail.com>
2022-02-08 11:31:17 -0500

    Create Customer and Transaction models

--- points/models.py
+++ points/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    points = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    payer = models.ForeignKey(
+        Customer, related_name="transactions", on_delete=models.CASCADE
+    )
+    points = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
