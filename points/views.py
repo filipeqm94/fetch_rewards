740d7cca9d1bc7baf10ed5250e0efddc5aaa5fdf
filipeqm94 <queiroz.filipe94@gmail.com>
2022-02-10 17:46:04 -0500

    Create spend route with business logic and send right response

--- points/views.py
+++ points/views.py
@@ -3,19 +3,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Customer, Transaction
 
-"""
-{ "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" }
-{ "payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z" }
-{ "payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z" }
-{ "payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z" }
-{ "payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z" }
-
-{ "points": 5000 }
-"""
-
 # Create your views here.
-
-
+# decorator to bypass the neccessity for the csrf token
 @csrf_exempt
 def transaction(request):
     data = json.loads(request.body)
@@ -34,7 +23,9 @@ def transaction(request):
     # save changes made to customer
     customer.save()
 
-    transaction = Transaction(payer=customer, points=data["points"])
+    transaction = Transaction(
+        payer=customer, points=data["points"], timestamp=data["timestamp"]
+    )
     transaction.save()
 
     data = {"detail": "transaction created"}
@@ -42,19 +33,60 @@ def transaction(request):
     return JsonResponse(data)
 
 
+# decorator to bypass the neccessity for the csrf token
+@csrf_exempt
 def spend(request):
-    data = {"hello": "spend"}
+    data = json.loads(request.body)
+    points = data["points"]
 
-    return JsonResponse(data)
+    # get transactions ordered by date
+    transactions = list(Transaction.objects.all().order_by("timestamp"))
+    # this list will hold the customer affected by the spend call
+    customers = []
+
+    # deduct points
+    for transaction in transactions:
+        # points are 0 or below break out
+        if points <= 0:
+            break
+        # subtract transaction points
+        points = points - transaction.points
+        # set deduceted points to always be a positive integer
+        deducted_points = transaction.points if points > 0 else transaction.points + points
+
+        # get customer related to the transaction
+        customer = Customer.objects.get(pk=transaction.payer.pk)
+        # update customer points
+        customer.points -= deducted_points
+        # save changes
+        customer.save()
+
+        # in order to create a list with only unique customers
+        if customer not in customers:
+            # set customer points to be the amount subtracted
+            # determined by the transactions
+            customer.points = deducted_points
+            # add unique customer to the list
+            customers.append(customer)
+        else:
+            # if customer was already on the list, add transaction points to the deducted points
+            customers[customers.index(customer)].points += deducted_points
+
+    # create response
+    # iterate through customers list creating a new list
+    # with the amount of points deducted from each customer
+    response = [{"payer": c.name, "points": -c.points} for c in customers]
+
+    return JsonResponse(response, safe=False)
 
 
 def balances(request):
     # get a list of all customers
     customers = list(Customer.objects.all())
-    data = {}
+    response = {}
     for customer in customers:
         # for each customer in the list,
         # add a "customer.name" key and assign "customer.points" as value
-        data[customer.name] = customer.points
+        response[customer.name] = customer.points
 
-    return JsonResponse(data)
+    return JsonResponse(response)
